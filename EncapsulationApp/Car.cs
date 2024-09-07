0f4fd22eb76dbc0265520200026b424d036a76c3
KAAN <alikaanozoll@gmail.com>
2024-09-07 10:00:16 +0300

    Add project files.

---
+++ EncapsulationApp/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationApp
+{
+    public class Car   // Car nesnelerinin tanımlanması ||Identifying car objects
+    {
+        private int _NumberofDoors;
+        public string Mark { get; set; }
+        public string Model { get; set; }  
+        public string Colour { get; set; }
+        public int NumberofDoors
+        {
+        get { return _NumberofDoors; }
+            set 
+            { 
+                if (value == 2 || value == 4)
+                {
+                    _NumberofDoors = value;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış kapı sayısı girdiniz. Sadece 2 veya 4 kapılı araba üretilir. Kapı değeri -1 olarak gösterilecektir.");
+                    _NumberofDoors = -1;
+                }
+            }
+        }
+        public Car(string mark, string model, string colour, int _numberofdoors)
+        {
+            Mark = mark;
+            Model = model;
+            Colour = colour;
+            NumberofDoors = _numberofdoors;
+        }
+
+    }
+    
+}
---
+++ EncapsulationApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using EncapsulationApp;
+
+Car car = new Car("Opel","Corsa","Beyaz",4);  //// Car sınıfından yeni bir nesne oluşturuyoruz. || We create a new object from the Car class.
+
+Console.WriteLine($"Car: {car.Mark}, {car.Model}, {car.Colour},kapı sayısı {car.NumberofDoors}");
